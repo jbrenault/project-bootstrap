2222315dc0037b0d94ec42f6c9a59f6258235f6f
jbrenault <renault.jeanbaptiste@gmail.com>
2019-02-27 10:18:17 +0100

    chore: add first bootstraps

---
+++ springboot-elk/src/main/java/org/toucan/elk/SpringBootElkApplication.java
@@ -0,0 +1,12 @@
+package org.toucan.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringBootElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootElkApplication.class, args);
+    }
+}
